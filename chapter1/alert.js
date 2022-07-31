300c0cd1557fd8745212dab57ef557294571fa34
scottschmidl <sjschmidl@gmail.com>
2022-07-30 21:31:08 -0500

    finished chapter 2

--- chapter1/alert.js
+++ chapter1/alert.js
@@ -1,1 +1,6 @@
-alert("thanks for your input!")
+// an alert is a box that pops up to give the user a message
+alert("thanks for your input!"); // this line didn't need a semicolon
+
+// a semicolon is always necessary, but it's easier to nd every statement with a semicolon, rathter than stop to figure out whether you need one.
+
+
---
+++ chapter2/variables_for_string.js
@@ -0,0 +1,19 @@
+var name= "Scott";
+name="Ace";
+
+var natinoality;
+natinoality = "U.S.";
+
+var floogle = "Scott";
+var name = "Scott";
+
+var lessonAuthor = "Scott";
+var guyWhoKeepsSayingHisOwnName = "Scott";
+var x = "Scott";
+
+var lastName = "Schmidl";
+var cityOfOrigin = "Point Pleasant";
+var aussieGreeting = " g'Day";
+
+
+alert(cityOfOrigin);
