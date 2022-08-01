3e355b6c9ee8c0d64d0b5dc9774c353e33686e01
scottschmidl <sjschmidl@gmail.com>
2022-07-31 23:53:20 -0500

    finished with chapter 3

---
+++ chapter3/variables_for_numbers.js
@@ -0,0 +1,21 @@
+var weight = 150;
+
+var new_weight = weight + 25;
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+
+originalNum = originalNum + 10;
+
+var primeNumberThatComes1st = 2;
+
+alert(144);
+
+var caseQty = 145;
+alert(caseQty);
+
+
