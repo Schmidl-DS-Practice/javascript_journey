16f84c590fa64e4f8d50ca630e2b6ce1e5bd0ae1
scottschmidl <sjschmidl@gmail.com>
2022-08-02 23:58:37 -0500

    finished chapter4 and added chapter5

---
+++ chapter4/var_names_legal_illegal.js
@@ -0,0 +1,25 @@
+/*
+RESERVED WORDS:
+abstract    final       protected
+alert       finally     public
+as          float       return
+boolean     for         short
+break       function    static
+byte        goto        super
+case        if          switch
+catch       implements  synchronized
+char        import      this
+class       in          throw
+continue    instanceof  throws
+const       int         transient
+debugger    interface   true
+default     is          try
+delete      long        typeof
+do          namespace   use
+double      native      var
+else        new         void
+enum        null        volatile
+export      package     while
+extends     private     with
+false
+*/
