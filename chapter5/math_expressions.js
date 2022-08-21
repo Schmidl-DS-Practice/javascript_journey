a9ededd8d56980aae5629bdef644fde8f4294273
scottschmidl <sjschmidl@gmail.com>
2022-08-20 23:26:24 -0500

    finished chapter 5

---
+++ chapter5/math_expressions.js
@@ -0,0 +1,23 @@
+var popularNumber = 4;
+
+var popularNumber = 2 + 2;
+
+alert(2+2);
+
+var popularNumber = 12 - 24;
+
+var popularNumber = 3 * 12;
+
+var popularNumber = 12 / 4;
+
+var num = 10;
+
+var popularNumber = num + 200;
+
+var num = 10;
+var another_num = 1;
+var popularNumber = num + another_num;
+
+var whats_left_over = 10 % 3;
+
+var whats_leftover = 9 % 3;
