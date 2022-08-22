a995e578b3965a72c2ede627a4765d1ab22281af
scottschmidl <sjschmidl@gmail.com>
2022-08-22 00:56:30 -0500

    finished chapter 10

---
+++ chapter10/if.js
@@ -0,0 +1,14 @@
+var x = prompt("what does a dog wag?");
+if (x === "tail") {
+    alert("correct!");}
+
+var correctanswer = "tail";
+if (x === correctanswer){
+    alert('correct!');}
+
+var correctans = 'tail';
+if (x === correctans){
+    score++;
+    useriq = 'genius';
+    alert('correct!');}
+
