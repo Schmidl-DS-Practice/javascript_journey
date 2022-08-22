1a9bd9c329a036ed485e92f73da6b9d615dc3b95
scottschmidl <sjschmidl@gmail.com>
2022-08-22 00:40:45 -0500

    finished chapter 8

---
+++ chapter8/concat_text_strings.js
@@ -0,0 +1,24 @@
+alert('thanks for your input!');
+var message = 'thanks for your input';
+alert(message)
+
+alert('thanks, ' + username + '!');
+
+var message = 'thanks, ';
+var banger = '!';
+alert(message + username + banger);
+
+alert("thanks, " + "susan" + "!");
+
+var message = 'thanks, ';
+var username = "susan";
+var banger = '!';
+
+var custommess = message + username + banger;
+
+alert(custommess);
+
+alert("2" + "2");
+
+alert("2 plus 2 equals" + 2 + 2);
+
