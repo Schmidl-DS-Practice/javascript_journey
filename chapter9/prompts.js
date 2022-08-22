f6887c7e93a29b26ee894d657cc1c9474e1b3f68
scottschmidl <sjschmidl@gmail.com>
2022-08-22 00:47:09 -0500

    finished chapter 9

---
+++ chapter9/prompts.js
@@ -0,0 +1,10 @@
+var spec = prompt("your species?", "human");
+
+var question = "your species?";
+var defaultanswer = 'human';
+var spec = prompt(question, defaultanswer);
+
+var numofcats = prompt("how many cats?");
+var toomanycats = numofcats + 1;
+
+// window.prompt
