eb3b09cdb7b153a8956fc0d97e5e4ee053eba8f9
scottschmidl <sjschmidl@gmail.com>
2022-08-23 00:13:11 -0500

    complete

---
+++ chapter11/comparison_operators.js
@@ -0,0 +1,28 @@
+// === is a comarison operator > equality operator
+
+if (fullname === "Mark" + " " + "Myers") {}
+if (fullname === firstname + " " + "myers"){}
+if (fullname === firstname + " " + lastname){}
+if (totalcost === 81.50 + 125){}
+if (totalcost === materialscost + 135){}
+if (totalcost === matericalcost + laborcost){}
+
+// !== is the opposite of ===
+
+if (yourticketnumber !== 487208){
+    alert("better luck next time");}
+
+/*
+other comparison operators:
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal too
+*/
+
+if (1 > 0){}
+if (0 < 1){}
+if (1 >= 0){}
+if (1 >= 1){}
+if (0 <= 1){}
+if (1 <= 1){}
