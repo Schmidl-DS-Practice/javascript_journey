087c5201cdd727eb68fe86fe7baa2baf0cb5e2f3
scottschmidl <sjschmidl@gmail.com>
2022-08-23 00:22:55 -0500

    finished

---
+++ chapter12/if_else_elseif.js
@@ -0,0 +1,44 @@
+//
+var x = prompt("what does a dog wag?");
+if (x === "tail"){
+
+    alert("correct!");}
+
+//
+var x = prompt("what does a dog wag?");
+if (x === "tail"){
+
+    alert("correct!");}
+
+if (x !== "tail"){
+    alert("wrong answer")}
+
+//
+var x = prompt("what does a dog wag?");
+
+if (x === "tail"){
+    alert("correct!");}
+
+else {
+    alert("wrong answer");}
+
+//
+var correctanswer = "tail";
+if (x === correctanswer){
+    alert("correct!");}
+
+else {
+    score --;
+    askagain = "yes";
+    alert("incorrect");}
+
+//
+var correctanswer = "tail";
+if (x === correctanswer){
+    alert("correct!");}
+
+else if (x === "tale"){
+    alert("incorrect but close");}
+
+else {
+    alert(incorrect);}
