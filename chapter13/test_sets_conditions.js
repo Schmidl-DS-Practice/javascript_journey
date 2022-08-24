1de61a5200285ad84eef78e8cfd49dcd910ee259
scottschmidl <sjschmidl@gmail.com>
2022-08-23 23:18:21 -0500

    finished

---
+++ chapter13/test_sets_conditions.js
@@ -0,0 +1,8 @@
+if (weight > 300 && time < 6){alert ("come to our tryout");}
+else {alert("come to our cookout");}
+
+// you can chain conditions (comparisons) together
+if (sat > avg || gpa > 2.5 || sport === "football") {alert("welcome to the bubba state!");}
+else {alert("have you looked into appliance repair?")}
+
+// use parenthesis when combining && (and) and || (or)
