651a3ca2531e5afaea50b8845c84b22bfccfa8c2
scottschmidl <sjschmidl@gmail.com>
2022-08-25 15:18:21 -0500

    finished

---
+++ chapter14/nested_if.js
@@ -0,0 +1,17 @@
+if ((x === y || a === b) && c === d) {g = h;}
+else {e = f;}
+
+if (c === d) {
+
+    if (x === y) {g = h;
+    }
+
+    else if (a === b) {g = h;
+    }
+
+    else {e = f;
+    }
+}
+
+else {e = f;
+}
