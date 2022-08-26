f53ccfa21e3c11a9b5757e41700910e3e2276a48
scottschmidl <sjschmidl@gmail.com>
2022-08-25 23:13:06 -0500

    finished

---
+++ chapter15/arrays.js
@@ -0,0 +1,14 @@
+var city0 = "atlanta";
+var city1 = "baltimore";
+var city2 = "chicago";
+var city3 = "denver";
+var city4 = "los angeles";
+var city5 = "seattle";
+
+alert("welcome to " + city3);
+
+var cities = ["atlanta", "baltimore", "chicago", "denver", "los angeles", "seattle"];
+
+alert("welcome to " + cities[3]);
+
+var mixarray = [1, "bob", "now is", true];
