f2be375bc292549a152e992e05c66d3b8537e1db
scottschmidl <sjschmidl@gmail.com>
2022-08-27 00:20:02 -0500

    finished

---
+++ chapters10_19/chapter18/for_loops.js
@@ -0,0 +1,9 @@
+var cleanest_cities = ["cheyenne", "santa fe", "tucson", "great falls", "honolulu"];
+
+
+// variable that counts iterations, and also serves as the array index, is declared and set to a starting value
+// limit on the loop
+// what happens at the end of every loop
+for (var i = 0; i <= 4; i++) {
+    if (cityToCheck === cleanest_cities[i]){
+        alert("it's one of the cleanest cities");}}
