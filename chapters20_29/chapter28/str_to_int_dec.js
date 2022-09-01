6a0a9d845391a0568067861ce55ea5f3f5442f76
scottschmidl <sjschmidl@gmail.com>
2022-08-31 22:06:58 -0500

    finished

---
+++ chapters20_29/chapter28/str_to_int_dec.js
@@ -0,0 +1,13 @@
+var current_age = prompt("enter your age: ");
+// js converts multiple string numbers to int and automatically does math
+
+// cant subtract string number and string word > NaN
+
+// "200" + 150 = "200150"
+// if -*/ js converts "200" operator 150 -> 200 operator 150
+
+var current_age = prompt("enter age: " );
+var qualifying_age = parseInt(current_age) + 1;
+
+var my_int = parseInt("1.9999"); // equals 1
+var my_floast = parseFloat("1.999"); //equals 1.999
