4356edcc4e4a832dcb69d6a13f96eda2ed321473
scottschmidl <sjschmidl@gmail.com>
2022-09-18 15:19:21 -0500

    f 46

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -216,6 +216,14 @@ for the same reason that it's a good idea to put js code at the end of the body 
 // /* */
 
 // Chapter 45
+// inline event handling
+// <a href="#" onClick="alert('Hi');">Click</a> // "#" reload the page, use below
+
+// <a href="JavaScript.void(0)" onClick="alert('Hi');">Click</a>
+
+// <a href="JavaScript.void(0)" onClick="var greet='hi'; alert(greet);">Click</a>
+
+// <a href="JavaScript.void(0)" onClick="popup('hi');">Click</a>
 
 // Chapter 46
 
