274bd20c571fa0e3700836a4381410cc59996768
scottschmidl <sjschmidl@gmail.com>
2022-09-18 17:50:44 -0500

    f64

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -445,8 +445,18 @@ if (targetNode.nodeName.toLowerCase() === "img"){
 }
 
 // Chapter 64
+for (var i = 0; i < howManyLi; i++){
+    if (liElements[i].innerHTML == "") {
+        liElements[i].innerHTML = "coming Soon";
+    }
+}
 
-
+var numPics = 0;
+for (var i = 0; i < howManyChildren; i++){
+    if (nodelist[i].nodeName.toLowerCase() === "img"){
+        numPics++;
+    }
+}
 // Chapter 65
 
 
