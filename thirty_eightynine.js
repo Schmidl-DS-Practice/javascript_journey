9aa07fb3672934c2e0ea3ef3910507043bc651bb
scottschmidl <sjschmidl@gmail.com>
2022-09-18 14:24:49 -0500

    finished 36

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -68,6 +68,26 @@ function telltime(){
 }
 
 // Chapter 36
+function greet_user() {
+    alert("Hello");
+}
+
+function greet_user(greeting){
+    alert(greeting);
+}
+
+greet_user("hello there")
+var greeting = "hello there";
+greet_user(greeting);
+
+var almostamil = 999999;
+showbignum(almostamil);
+
+function showmessage(m, string, num){
+    alert (m + string + num);
+}
+var month = "March";
+showmessage(month, "'s winning number is ", 23);
 
 // Chapter 37
 
