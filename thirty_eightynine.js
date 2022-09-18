bb213809953d598ee7ce002051b1fdad389fae85
scottschmidl <sjschmidl@gmail.com>
2022-09-18 16:31:59 -0500

    f54

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -350,7 +350,20 @@ function swapPic(eId, newpic){
 }
 
 // Chapter 54
+function swapPic(){
+    var pic = document.getElementById("before");
+    pic.src = "after-pic.jpg";
+}
 
+function getAddress(){
+    var link = document.getElementById("link1");
+    var address = link.href;
+}
+
+function enLargeForm(){
+    var frm = document.getElementById("f12");
+    frm.className = "jumbo";
+}
 
 // Chapter 55
 
