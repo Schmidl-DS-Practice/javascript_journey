9312710d1ff2a64fef91ea3bbc459b33af6b11cb
scottschmidl <sjschmidl@gmail.com>
2022-09-18 16:28:37 -0500

    f 53

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -340,7 +340,14 @@ function makebig(){
 }
 
 // Chapter 53
-
+// <img src="" id="" onMouseover="swapPic();>"
+function swapPic(){
+    document.getElementById("before").src = "after-pic.jpg";
+}
+// <img .. onMouseover="swapPic(id, 'after-pic.jpg);">
+function swapPic(eId, newpic){
+    document.getElementById(eId).src = newpic;
+}
 
 // Chapter 54
 
