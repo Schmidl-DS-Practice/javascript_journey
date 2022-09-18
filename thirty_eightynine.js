8425043148bbaf18094ce0ac75ec6c14c3a04e8f
scottschmidl <sjschmidl@gmail.com>
2022-09-18 14:58:09 -0500

    finished 42

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -178,6 +178,17 @@ while (i <= 3){
 }
 
 // Chapter 42
+var i = 0;
+while (i <=3){
+    alert(i);
+    i++;
+}
+
+var i = 0;
+do { // always executes the code block once
+    alert(i);
+    i++;
+} while (i <=3);
 
 // Chapter 43
 
