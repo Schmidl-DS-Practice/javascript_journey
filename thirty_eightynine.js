6ca2db3c801e3743feb122827e9a826d06a17092
scottschmidl <sjschmidl@gmail.com>
2022-09-18 14:32:16 -0500

    finished 37

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -90,6 +90,29 @@ var month = "March";
 showmessage(month, "'s winning number is ", 23);
 
 // Chapter 37
+function calctot(merchtot){
+    var ordertot;
+    if (merchtot >= 100){
+        ordertot = merchtot;
+    }
+    else if (merchtot < 50.01){
+        ordertot = merchtot + 5;
+    }
+    else {
+        ordertot = merchtot + 5 + (0.03 * (merchtot - 50));
+    }
+    return ordertot;
+}
+var totaltocharge = calctot(79.99)
+alert (calctot(79.00));
+
+var ordertot = merchtot + calctax(merchtot);
+
+var tot = calc(merchtot, calctax(merchtot));
+
+function calc_tot(price){
+    return price + calc_ship(price);
+}
 
 // Chapter 38
 
