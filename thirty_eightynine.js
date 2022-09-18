a9bfdd8d974e980ff7d71e19e1a9eced45a90a30
scottschmidl <sjschmidl@gmail.com>
2022-09-18 17:41:24 -0500

    f62

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -412,7 +412,19 @@ var p = d.childNodes[1];
 var contents = p.innerHTML;
 
 // Chapter 61
+var nType = targetNode.nodeType;
 
+var d = document.getElementById("humpty");
+var pCounter = 0;
+for (var i = 0; i < d.childNodes.length; i++){
+    if (d.childNodes[i].nodeType === 1){
+        pCounter++;
+    }
+    if (pCounter === 2){
+        d.childNodes[i].innerHTML = "all his men.";
+        break;
+    }
+}
 
 // Chapter 62
 
