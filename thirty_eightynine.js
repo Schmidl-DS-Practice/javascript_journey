8a89add0c98135cc9ff3b96dbaf24244f09edf2f
scottschmidl <sjschmidl@gmail.com>
2022-09-18 18:51:31 -0500

    f70

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -510,7 +510,15 @@ var plan1 = {
 plan1.discount_months = [6,7,11];
 
 // Chapter 70
+deal3.cost = 79.95;
+deal3.features = ["guara", "free"]
+deal3.members_only = false;
+deal3.market = undefined;
+delete deal3.market;
 
+var deal4 = {};
+
+propertyExists = "market" in deal3;
 
 // Chapter 71
 
