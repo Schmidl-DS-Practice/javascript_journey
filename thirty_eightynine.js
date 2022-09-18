6decb9dbcb3edd92b0296291be1371701ec4eb9d
scottschmidl <sjschmidl@gmail.com>
2022-09-18 18:00:14 -0500

    f68

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -486,9 +486,15 @@ document.createElement("img");
 document.createTextNode("");
 nodeToAdd.appendChild(newText);
 
-
 // Chapter 68
+para1 = parentDiv.firstChild;
+parentDiv.insertBefore(newPara, para1);
 
+// no insertAfter
+var target = parentDiv.childNode[1];
+parentDiv.insertBefore(newE, target.nextSibling);
+
+parentDiv.removeChild(noteToRemove);
 
 // Chapter 69
 
