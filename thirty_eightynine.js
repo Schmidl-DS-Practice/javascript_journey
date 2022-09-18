da56599867826741671279c74b5bd889f900639a
scottschmidl <sjschmidl@gmail.com>
2022-09-18 16:39:55 -0500

    f56

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -376,7 +376,16 @@ document.getElementById("mainpic").style.margin = "0 10px 0 10 px";
 var m = window.getComputedStyle("mainpic").margin;
 
 // Chapter 56
+getElementById(th_id).style.fontSize = "2em";
+var par = document.getElementsByTagName("p"); // index into par for each paragraph
+var textinmiddleparagraph = par[1].innerHTML;
 
+for (var i = 0; i < par.length; i++){
+    par[i].style.fontFamily = "Verdana, Geneva, sans-serif"
+}
+var pics = document.getElementsByTagName("img");
+var divs = document.getElementsByTagName("div");
+var ulists = document.getElementsByTagName("ul")
 
 // Chapter 57
 
