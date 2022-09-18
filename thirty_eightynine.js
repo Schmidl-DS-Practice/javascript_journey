bc028ca9de17e20e3dffe228c6e1617d6ce4eb31
scottschmidl <sjschmidl@gmail.com>
2022-09-18 14:52:41 -0500

    finished 40

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -152,6 +152,19 @@ default:
 }
 
 // Chapter 40
+switch (dayofwk){
+case "Sat":
+    alert("whoopee");
+    break;
+case "Sun":
+    alert("cool");
+    break;
+case "Fri":
+    alert("tgif");
+    break;
+default:
+    alert("ugh2");
+}
 
 // Chapter 41
 
