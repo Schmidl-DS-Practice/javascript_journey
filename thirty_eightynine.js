7fb6aa82c255bca17c05defedcf1e3261d1d869c
scottschmidl <sjschmidl@gmail.com>
2022-09-18 15:39:23 -0500

    f 49

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -249,6 +249,37 @@ for the same reason that it's a good idea to put js code at the end of the body 
 // <input type="text" size="30" onFocus="makefieldyellow();" onBlur="makefieldwhite();">
 
 // Chapter 49
+/*
+<form>
+    Email:
+    <input type="text">
+    <input type="submit" value="Submit">
+</form>
+
+<form>
+    Email:
+    <input type="text" id="email">
+    <input type="submit" value="Submit">
+</form>
+
+<form onSubmit="checkAddress('email');">
+    Email:
+    <input type="text" id="email">
+    <input type="submit" value="Submit">
+</form>
+*/
+function checkAddress(fieldId){
+    if (document.getElementById(fieldId).value === ""){
+        alert("email address requrie");
+    }
+}
+
+function checkAddress(fieldId){
+    var val = document.getElementById(fieldId).value;
+    if (val === ""){
+        alert("email address requrie");
+    }
+}
 
 // Chapter 50
 
