b2e25f0279e07f13e55caac3646d9f783c6ccd52
scottschmidl <sjschmidl@gmail.com>
2022-09-18 16:54:29 -0500

    f58

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -398,7 +398,7 @@ for (var i = 0; i < cells.length; i++){
 }
 
 // Chapter 58
-
+// DOM (document object model) > organization chart, created automatically by the browser when your web page loads, for the whole web page.
 
 // Chapter 59
 
