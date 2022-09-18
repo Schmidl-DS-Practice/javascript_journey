a991a41d7f9c914a0f477ee538b4e8fe5ef1041e
scottschmidl <sjschmidl@gmail.com>
2022-09-18 16:25:16 -0500

    f52

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -326,7 +326,18 @@ function peekatcontent(){
 }
 
 // Chapter 52
-
+// give image id
+// add event handler
+// use CSS class for invisibility: .hidden {display:none;}
+function makeinvisible(){
+    document.getElementById("ugly").className="hidden";
+}
+// .value (form fields)
+// .innerHTML (paragraphs, divs, other HTML elements
+// .className (replaces any classes assigned in static markup)
+function makebig(){
+    document.getElementById("p1").className += " big";
+}
 
 // Chapter 53
 
