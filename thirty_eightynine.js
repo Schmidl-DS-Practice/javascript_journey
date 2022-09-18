f704a2ae19a9fcb11a5d8a996d4340fcea38dc74
scottschmidl <sjschmidl@gmail.com>
2022-09-18 15:29:33 -0500

    f 47

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -232,6 +232,11 @@ for the same reason that it's a good idea to put js code at the end of the body 
 // <img src="button-sum.png" onClick="greeting();">
 
 // Chapter 47
+// <img src="before-pic.jpg" onMouseover="src='after-pic.jpg'">
+// <h1 onMouseover="alert('');">World Ends</h1>
+// <a href="index.html" onMouseover="this.style.color = 'green';">Home Page</a>
+// <p id="loris" onMouseover="expand();">Slow Loris: Mouse over for more info</p>
+// <img src="before-pic.jpg" onMouseover="src='after-pic.jpg'" onMouseout="src='before-pic.jpg'">
 
 // Chapter 48
 
