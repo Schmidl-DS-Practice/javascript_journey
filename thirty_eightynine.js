db0f65fde863ac065bd83dc203911986e6a3a028
scottschmidl <sjschmidl@gmail.com>
2022-09-18 16:19:51 -0500

    f51

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -307,7 +307,23 @@ function fillCity(){
 }
 
 // Chapter 51
+/*
+<p id="slowloris">
+text. <a href="javascript:void(0);" onClick="expandLoris();"> Click for more.</a>
+*/
+function explandLoris(){
+    var expanded = "some text";
+    document.getElementById("slowLoris").innerHTML = expanded;
+}
 
+function placeAlist() {
+    var listtoplace = "<ol><li>slow loris</li><li>fast loris</li><li>just-right loris</li></ol>";
+    document.getElementById("lorislist").innerHTML = listtoplace;
+}
+
+function peekatcontent(){
+    var whatsthere = document.getElementById("content").innerHTML;
+}
 
 // Chapter 52
 
