de683b68c691c79bd11ea1ac666557220ec052d8
scottschmidl <sjschmidl@gmail.com>
2022-09-18 14:46:33 -0500

    finished 39

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -127,6 +127,29 @@ function addnumber(){
 addnumbers();
 
 // Chapter 39
+if (dayofwk === "Sat" || dayofwk === "Sun"){
+    alert("whoopee");
+}
+else if (dayofwk === "Fri"){
+alert("tgif");
+}
+else{
+alert("ugh");
+}
+
+switch (dayofwk){
+case "Sat":
+    alert("whoopee");
+    break;
+case "Sun":
+    alert("cool");
+    break;
+case "Fri":
+    alert("tgif");
+    break;
+default:
+    alert("ugh2");
+}
 
 // Chapter 40
 
