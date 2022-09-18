d66c53aba3f34ef784f9ec5c1a144adb10f0d1dd
scottschmidl <sjschmidl@gmail.com>
2022-09-18 16:35:25 -0500

    f55

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -366,7 +366,14 @@ function enLargeForm(){
 }
 
 // Chapter 55
+function makebig(){
+    document.getElementById("p1").style.fontSize = "2em";
+}
 
+document.getElementById("pic99").style.cssFloat = "left";
+document.getElementById("div9").style.visibility = "hidden";
+document.getElementById("mainpic").style.margin = "0 10px 0 10 px";
+var m = window.getComputedStyle("mainpic").margin;
 
 // Chapter 56
 
