a5fedce760c324755bcb7785057e8b8d4a1e556e
scottschmidl <sjschmidl@gmail.com>
2022-09-18 14:17:41 -0500

    finished 35

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -55,6 +55,17 @@ setMilliseconds(); // pass a number
 */
 
 // Chapter 35
+var now = new Date();
+var thehr = now.getHours();
+var themin = now.getMinutes();
+alert("time: " + thehr + ":" + themin);
+
+function telltime(){
+    var now = new Date();
+    var thehr = now.getHours();
+    var themin = now.getMinutes();
+    alert("time: " + thehr + ":" + themin);
+}
 
 // Chapter 36
 
