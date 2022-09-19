818cba80da1b02b5e7078d3acf5be65e1dd06446
scottschmidl <sjschmidl@gmail.com>
2022-09-18 20:11:33 -0500

    finished all

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -899,3 +899,19 @@ function checkPassword(){
 }
 
 // Chapter 89
+
+// <input type="button" value="Click" id="button1">
+var b1 = document.getElementById("button1");
+b1.onclick = sayHello;
+
+document.getElementById("button1").onclick = sayHello;
+
+function sayHello(){
+    alert("hi");
+}
+
+var targetimg = document.getElementById("i12");
+targetimg.onmouseover = swapPic;
+
+var emailFrm = document.getElementById("form5");
+emailFrm.onsubmit = valEmail;
