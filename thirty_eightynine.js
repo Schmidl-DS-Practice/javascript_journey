437219fcb06f82087ae0d35f5a54b9709a46bc91
scottschmidl <sjschmidl@gmail.com>
2022-09-18 19:21:46 -0500

    f81

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -706,7 +706,21 @@ windowspec = "'blah.html', 'fag', 'width=420,height=380,left=20,top=100'"
 var monkeywindow = window.open(windowspec);
 
 // Chapter 81
+function checkforpopupblocker(){
+    var testpop = window.open("", "", "");
+    if (testpop === null){
+        alert("please disable your popup blocker");
+    }
+    testpop.close();
+}
 
+function checkforpopupblocker(){
+    var testpop = window.open("", "", "");
+    if (testpop === null || typeof(testpop) === "undefined"){
+        alert("please disable your popup blocker");
+    }
+    testpop.close();
+}
 
 // Chapter 82
 
