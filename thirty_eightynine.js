dd6c66a9f9bfa5c23f4a903f43ed86772d6821a2
scottschmidl <sjschmidl@gmail.com>
2022-09-18 19:06:05 -0500

    f74

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -633,7 +633,21 @@ function Plan(name, price, space, transfer, pages, discount_months){
 this.calcAnnual = function(percentOfDisc){}
 
 // Chapter 74
+Plan.prototype.calcAnnual = function(percentOfDisc){
+    var bestPrice = this.price;
+    var currDate = new Date();
+    var theMo = currDate.getMonth();
+    for (var i = 0; i < this.discount_months.length; i++){
+        if (plan1.discount_months[i] === theMo){
+            bestPrice = this.price * percentOfDisc;
+            break;
+        }
+    }
+    return bestPrice * 12;
+};
 
+Plan.prototype.cancellable = true;
+plan1.cancellable = false;
 
 // Chapter 75
 
