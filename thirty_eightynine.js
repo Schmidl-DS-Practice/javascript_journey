13b9194069ab76b619f53318df8275530cc98a40
scottschmidl <sjschmidl@gmail.com>
2022-09-18 19:19:41 -0500

    f80

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -698,7 +698,12 @@ monkeywindow = window.open();
 monkeywindow.document.write(windowcontent)
 
 // Chapter 80
+var monkeywindow = window.open("monk.html", "win1");
+var monkeywindow = window.open("monk.html", "win1", "width=420, height=380")
+var monkeywindow = window.open("", "", "width=420,height=380,left=20,top=100");
 
+windowspec = "'blah.html', 'fag', 'width=420,height=380,left=20,top=100'"
+var monkeywindow = window.open(windowspec);
 
 // Chapter 81
 
