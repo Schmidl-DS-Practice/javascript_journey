297835c7a775e10a7216512aa35adea97f9473cd
scottschmidl <sjschmidl@gmail.com>
2022-09-18 19:32:10 -0500

    f83

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -755,7 +755,26 @@ function checkforlastname(){2
 }
 
 // Chapter 83
-
+/*
+<form onSubmit="return checkforselection();">
+    <select id="states">
+        <option value="" selected="selected">
+            SELECT A STATE</option>
+                <option value="al">alabama</option>
+                <option value="ak">alaska</option>
+                <option value="az">arizona</option>
+                <option value="ar">arkansas</option>
+    </select>&nbsp;&nbsp;
+    <input type="submit" value="Submit Form">
+</form>
+*/
+function checkforselection(){
+    var targestindex = document.getElementById("states");
+    if (targestindex.selectedIndex == 0){
+        alert("select")
+        return false:
+    }
+}
 
 // Chapter 84
 
