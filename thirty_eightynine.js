9d405f3a16d1a6ac2ee843c76b7307f3ddc5db1a
scottschmidl <sjschmidl@gmail.com>
2022-09-18 19:12:03 -0500

    f76

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -665,8 +665,17 @@ for (var prop in plan1){
 }
 
 var isownedproperty = plan1.hasOwnProperty("price");
-// Chapter 76
 
+// Chapter 76
+var wheereweat = window.location.href;
+var thedomain = window.location.hostname;
+var thepath = window.location.pathname;
+var theanchor = window.location.hash;
+
+window.location.href = "https://...";
+
+// can omit window
+// can do document.URL instead of href
 
 // Chapter 77
 
