5222b0181ec92bf39fb0753892d153050c3b13c7
scottschmidl <sjschmidl@gmail.com>
2022-09-18 19:08:54 -0500

    f75

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -650,8 +650,21 @@ Plan.prototype.cancellable = true;
 plan1.cancellable = false;
 
 // Chapter 75
+var gottheproperty = "price" in plan1;
+var gottheproperty = "calcAnnual" in plan1;
 
+var listofproperties = [];
+for (var prop in plan1){
+    listofproperties.push(prop);
+}
+var listofproperties = [];
+for (var prop in plan1){
+    if (plan1.hasOwnProperty(prop)){
+        listofproperties.push(prop);
+    }
+}
 
+var isownedproperty = plan1.hasOwnProperty("price");
 // Chapter 76
 
 
