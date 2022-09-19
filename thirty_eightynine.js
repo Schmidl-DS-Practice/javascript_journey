383c4ca472801de0a96f0277944e3d8fb123b0e3
scottschmidl <sjschmidl@gmail.com>
2022-09-18 19:50:54 -0500

    f85

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -810,9 +810,32 @@ function validateradios(ename){
 
 /*
 <form onSubmit="return validateradios('r1');">
+*/
 
 // Chapter 85
+function validatezip(){
+    var numchars = document.getElementById("zip").value.length;
+    if (numchars < 5) {
+        alert("enter 5");
+        return false
+    }
+}
 
+function validatezip(){
+    var valueEntered = document.getElementById("zip").value;
+    var numchars = valueEntered.length;
+    if (numchars < 5) {
+        alert("enter 5");
+        return false
+    }
+    for (var i = 0; i <= 4; i++){
+        var thisChar = parseInt(valueEntered[i]);
+        if (isNaN(thisChar)){
+            alert('only numbers');
+            return false;
+        }
+    }
+}
 
 // Chapter 86
 
@@ -824,4 +847,3 @@ function validateradios(ename){
 
 
 // Chapter 89
-
