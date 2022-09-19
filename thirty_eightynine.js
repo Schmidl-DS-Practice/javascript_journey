3698019e570a18db1499450c0142aa9f06e27780
scottschmidl <sjschmidl@gmail.com>
2022-09-18 19:26:21 -0500

    f82

--- thirty_eightynine.js
+++ thirty_eightynine.js
@@ -723,7 +723,36 @@ function checkforpopupblocker(){
 }
 
 // Chapter 82
+/*
+<form onSubmit = "return checkforlastname();">
+*/
+function checkforlastname(){
+    if (document.getElementById("lastnamefield").value.length === 0){
+        alert("enter last name");
+        document.getElementById("lastnamefield").focus();
+        return false;
+    }
+}
 
+function checkforlastname(){
+    var targetfield = document.getElementById("lastnamefield");
+    if (targetfield.value.length === 0){
+        alert("enter last name");
+        targetfield.focus();
+        return false;
+    }
+}
+
+function checkforlastname(){2
+    var targetfield = document.getElementById("lastnamefield");
+    if (targetfield.value.length === 0){
+        alert("enter last name");
+        targetfield.focus();
+        targetfield.style.background = "yellow";
+        return false;
+    }
+    targetfield.style.background = "white";10
+}
 
 // Chapter 83
 
